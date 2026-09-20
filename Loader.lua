00d1ebea116986bf9cd5700126f753b4d17810f9
hutohava <dalyccoy7788@gmail.com>
2026-09-20 18:20:12 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@
 
 local BASE_URL = "https://raw.githubusercontent.com/betdoyvaka/stealanegg/main/"
 
-_G.YOKUDO_EnablePrint = true
+_G.YOKUDO_EnablePrint = false
 
 local oldPrint = print
 print = function(...)
