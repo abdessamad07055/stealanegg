df4e85db3b3e00c55856c537b0c8e16ddd3556c3
hutohava <dalyccoy7788@gmail.com>
2026-09-23 08:40:56 +0700

    Update AttackDrone.lua

--- Features/AttackDrone.lua
+++ Features/AttackDrone.lua
@@ -18,7 +18,7 @@ local Player = Players.LocalPlayer
 -- ==================================================
 local ATTACK_RANGE = 16
 local ATTACK_INTERVAL = 0.02
-local FOLLOW_SPEED = 700
+local FOLLOW_SPEED = 350
 local FOLLOW_BEHIND_DISTANCE = 3
 local SHORT_TP_DISTANCE = 15   -- ✅ ប្តូរពី 20 ទៅ 15
 local SPAWN_POSITION_1 = Vector3.new(2140, 77, -367)
